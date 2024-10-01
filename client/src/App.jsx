df7c7cca2666941b77963a4b08adeb52ad0b5a44
UmerAbbasi-100 <k213929@nu.edu.pk>
2024-10-01 23:04:59 +0500

    initial setup

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-4xl font-bold text-center text-teal-400'>Hello World</h1>
+    </>
+  )
+}
+
+export default App
---
+++ server/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";    
+
+dotenv.config();
+
+const app = express();
+
+
+//mongo db connection
+const Mongo_uri=process.env.Mongo;
+
+if(!Mongo_uri) throw new Error("MongoDB connection string is not found");
+
+mongoose.connect(Mongo_uri).then(()=>console.log("MongoDB connected"))
+.catch((err)=>console.log(err));
+
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+}));
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT,()=>console.log(`server is running on port : ${PORT}` ));
