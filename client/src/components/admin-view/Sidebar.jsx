cf08e6680e9c3a811322c3bd6c2b9ff3a1402bc7
UmerAbbasi-100 <k213929@nu.edu.pk>
2024-10-22 23:26:50 +0500

    feature added

--- client/src/components/admin-view/Sidebar.jsx
+++ client/src/components/admin-view/Sidebar.jsx
@@ -1,8 +1,42 @@
+import { LayoutDashboard, ShoppingBasket,BadgeCheck } from "lucide-react";
+
+
+const adminSideBarMenu=[
+    {
+        id: "dashboard",
+        label:"Dashboard",
+        path:"/admin/Dashboard",
+        icon:<LayoutDashboard />
+
+
+     },
+     {
+        id:"features",
+        label:"features",
+        path:"/admin/features",
+        icon:<ShoppingBasket />
+     },
+
+     {
+        id:"products",
+        label:"Products",
+        path:"/admin/products",
+        icon:<ShoppingBasket />
+     },
+     {
+        id:"orders",
+        label:"Orders",
+        path:"/admin/orders",
+        icon:<BadgeCheck />
+     }
+
+]
+
 const Sidebar=()=>{
+    
     return(
-        <div>
-            <h1>Sidebar</h1>
-        </div>
+        <>
+        </>
     )
 }
 
